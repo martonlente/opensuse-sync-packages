2ff9a95c1d7be04da8adb4ee80d7e6201b20f657
Márton Lente <lentemarton@gmail.com>
2026-06-30 15:27:07 +0200

    Cleanup format script update-sync-packages-installed

--- update-opensuse-sync-packages-installed.sh
+++ update-opensuse-sync-packages-installed.sh
@@ -1,7 +1,11 @@
+# List packages installed
 zypper search --installed-only | \
+
 # Print table content (skip first 5 lines)
 tail -n +6 | \
+
 # Extract column 2 (package names)
 cut -d'|' -f2 | \
+
 # Remove trailing whitespace (process each line as single argument)
 xargs -n1 > opensuse-sync-packages-installed.txt
