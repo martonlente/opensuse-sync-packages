bb5f6c4f056c2b2f720c064098ae8394e192f385
Márton Lente <lentemarton@gmail.com>
2026-09-03 09:37:53 +0200

    Add script opensuse-sync-packages-install flag is-flatpak

--- opensuse-sync-packages-install.sh
+++ opensuse-sync-packages-install.sh
@@ -1,8 +1,24 @@
 #!/bin/sh
 
+is_flatpak=false
+
+# Check if arg is_flatpak is passed
+while [ "$#" -gt 0 ]; do
+  case "$1" in
+    --is-flatpak)
+      is_flatpak=true
+      shift
+      ;;
+    *)
+      # Read arg as src
+      break
+      ;;
+  esac
+done
+
 # Check if src arg is passed
 if [ -z "$1" ]; then
-  echo "Usage: $0 src=<packages.txt>"
+  echo "Usage: $0 [--is-flatpak] src=<packages.txt>"
   exit 1
 fi
 
@@ -15,8 +31,13 @@ if [ ! -f "$src" ]; then
   exit 1
 fi
 
-# Read package list from file
-cat "$src" | \
+# Read packages from src
+packages=$(cat "$src")
 
-# Format into a single line and install packages using zypper
-xargs zypper install
+if [ "$is_flatpak" = true ]; then
+  # Format into a single line and install packages using flatpak
+  echo "$packages" | xargs -I {} flatpak install flathub {} # Flag -I is needed to run install once per package
+else
+  # Install packages using zypper
+  echo "$packages" | xargs zypper install
+fi
